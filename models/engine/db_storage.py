0c8c269bc03f8af0f051db21443a499d1578eac2
root <Emdinga>
2023-10-20 02:53:27 -0700

    Flask

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -58,4 +58,6 @@ class DBStorage:
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)
 
-
+    def close(self):
+        """call remove() method on the private session attribute"""
+        self.__session.remove()
